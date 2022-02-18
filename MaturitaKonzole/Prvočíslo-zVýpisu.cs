aa5838aa0c8e3b18a73585a11d5dce06c0d4ce99
František Dvořák <dvorakml@garfen.cz>
2022-02-19 00:36:18 +0100

    Přidejte soubory projektu.

---
+++ MaturitaKonzole/Prvočíslo-zVýpisu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaturitaKonzole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ifrastruktura
+            Console.WriteLine("Zadejte číslo: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Číslo {0} není prvočíslo.", n);
+            }
+
+            else if (n == 3)
+            {
+                Console.WriteLine("Číslo {0} je prvočíslo.", n);
+            }
+            else if (n % 3 == 0)
+            {
+                Console.WriteLine("Číslo {0} není prvočíslo.", n);
+            }
+
+            else if (n % 5 == 0)
+            {
+                Console.WriteLine("Číslo {0} není prvočíslo.", n);
+            }
+
+            else Console.WriteLine("Číslo {0} je prvočíslo.", n);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MaturitaKonzole/Prvočíslo-zeSouboru.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MaturitaKonzole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamWriter sw = new StreamWriter(@"název souboru"))
+            {
+                int n;
+
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine(n);
+                }
+
+                Console.WriteLine("Tohle jsou prvočísla ze souboru: {0} ", n);
+            }
+
+        }
+    }
+}
