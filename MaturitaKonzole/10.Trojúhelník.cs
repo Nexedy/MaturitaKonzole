4a7206fb8001e777d013267f89ecdee9c10ee7ed
František Dvořák <dvorakml@garfen.cz>
2022-03-13 21:06:40 +0100

    automat na lístky

---
+++ MaturitaKonzole/10.Trojúhelník.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaturitaKonzole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Zadejte hodnotu a: ");
+            int a = int.Parse(Console.ReadLine());
+
+           Console.WriteLine("Zadejte hodnotu b: ");
+            int b = int.Parse(Console.ReadLine());
+
+           Console.WriteLine("Zadejte hodnotu c: ");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (a + b >= c)
+            {
+                Console.WriteLine("Trojúhleník existuje!");
+            }
+
+            else Console.WriteLine("Trojúhelník neexistuje!");
+
+            if (a > b)
+            {
+                a = c;
+                c = a;
+            }
+
+            else if (b > a)
+            {
+                b = c;
+                c = b;
+            }
+
+            else if (a > c)
+            {
+                a = c;
+                c = a;
+            }
+
+            else if (b > c)
+            {
+                b = c;
+                c = b;
+            }
+
+            int d = a * a;
+            int e = b * b;
+            int f = c * c;
+
+
+            //pravouhly kokotko
+            if (d + e = f)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ MaturitaKonzole/11. Automat na lístky-nope.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaturitaKonzole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Vyberte jakou vzdálenost chcete urazit: ");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("1. 1-3 km");
+            Console.WriteLine("2. 4-5 km");
+            Console.WriteLine("3. 5-7 km");
+            Console.WriteLine("4. >8 km");
+            Console.WriteLine("-----------------------------------------");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------------------------");
+
+            int b = Convert.ToInt32(a);
+
+            switch (b)
+            {
+                case 1:
+                    Console.WriteLine("Jízdenka");
+                    Console.WriteLine("----------");
+                    Console.WriteLine("Pásmo: 1");
+                    Console.WriteLine("Cena: 15.KČ");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Jízdenka");
+                    Console.WriteLine("----------");
+                    Console.WriteLine("Pásmo: 2");
+                    Console.WriteLine("Cena: 13.KČ");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Jízdenka");
+                    Console.WriteLine("----------");
+                    Console.WriteLine("Pásmo: 3");
+                    Console.WriteLine("Cena: 11.KČ");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Jízdenka");
+                    Console.WriteLine("----------");
+                    Console.WriteLine("Pásmo: 4");
+                    Console.WriteLine("Cena: 10.KČ");
+                    break;
+
+                default:
+                    Console.WriteLine("Nezadali jste hodnotu.");
+                    break;
+            }
+
+            Console.Read();
+
+
+        }
+    }
+}
